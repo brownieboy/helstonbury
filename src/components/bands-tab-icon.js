790fface791f2c6c1531bace1ca522e045e593cd
Michael Brown <brownieboy@gmail.com>
2018-01-26 13:00:35 +1100

    Renamed to bands-tab-icon.js

--- src/components/bands-tab-icon.js
+++ src/components/bands-tab-icon.js
@@ -2,17 +2,14 @@ import React from "react";
 import { Icon } from "native-base";
 
 // tintColor passed in is an object
-const BandsTabIcon = ({ tintColor }) => {
-  // console.log("BandsTabIcon tintColor = " + JSON.stringify(tintColor));
-  return (
-    <Icon
-      // name="guitar-acoustic"
-      ios="ios-musical-note-outline"
-      android="md-musical-notes"
-      size={30}
-      style={{ color: tintColor }}
-    />
-  );
-};
+const BandsTabIcon = ({ tintColor }) => (
+  <Icon
+    name="guitar-acoustic"
+    // ios="ios-musical-note-outline"
+    // android="md-musical-notes"
+    size={30}
+    style={{ color: tintColor }}
+  />
+);
 
 export default BandsTabIcon;

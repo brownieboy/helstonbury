3c87985988469ae71f43f71e2c7ad5f5edabdef0
Michael Brown <brownieboy@gmail.com>
2018-01-30 22:07:51 +1100

    Updated local data for appearance(s) times

--- src/dux/sagas.js
+++ src/dux/sagas.js
@@ -3,24 +3,6 @@ import { call, put, takeLatest } from "redux-saga/effects";
 import bandsApi from "../api/bandsApi.js";
 import { bandsDuxConstants } from "./bandsReducer.js";
 
-// worker Saga: will be fired on USER_FETCH_REQUESTED actions
-// function* loadBands(action) {
-//   try {
-//     const user = yield call(Api.fetchUser, action.payload.userId);
-//     yield put({ type: "USER_FETCH_SUCCEEDED", user: user });
-//   } catch (e) {
-//     yield put({ type: "USER_FETCH_FAILED", message: e.message });
-//   }
-// }
-/*
-export const bandsDuxConstants = {
-  LOAD_BANDS,
-  LOADED_BANDS_SUCCESS,
-  LOADED_BANDS_FAILURE,
-  LOADING_BANDS
-};
- */
-
 // worker Saga: will be fired on LOAD_BANDSactions
 function* loadBandsGen() {
   yield console.log("loadBands() triggered in sagas.js");
@@ -38,14 +20,6 @@ function* loadBandsGen() {
     });
   }
 }
-
-/*
-  Alternatively you may use takeLatest.
-
-  Does not allow concurrent fetches of user. If "USER_FETCH_REQUESTED" gets
-  dispatched while a fetch is already pending, that pending fetch is cancelled
-  and only the latest one will be run.
-*/
 
 function* mySaga() {
   yield takeLatest(bandsDuxConstants.LOAD_BANDS, loadBandsGen);

9b257140a1673e58b58a5901be91390e58c9a419
Michael Brown <brownieboy@gmail.com>
2018-02-20 18:46:28 +1100

    Build numbers to 31

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import configureStore from "./src/store/configureStore.js";
-// import SplashScreen from "react-native-splash-screen";
+import SplashScreen from "react-native-splash-screen";
+import { Platform } from "react-native";
 
 // import logo from "./logo.svg";
 
@@ -12,8 +13,11 @@ const store = configureStore({});
 class App extends Component {
   componentDidMount() {
     // do stuff while splash screen is shown
-    // After having done stuff (such as async tasks) hide the splash screen
-    // SplashScreen.hide();
+    // After having done stuff (such as async tasks) hide the splash screen for Android.
+    // We'll stick with Storyboard for iOS.
+    if (Platform.OS === "android") {
+      SplashScreen.hide();
+    }
   }
   render() {
     return (

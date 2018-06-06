3af799f22ab059a39cab92496b0dd9b4aece3d3d
Michael Brown <brownieboy@gmail.com>
2018-06-06 10:29:05 +1000

    Proper format for exporting sagas now

--- src/dux/sagas/rootSaga.js
+++ src/dux/sagas/rootSaga.js
@@ -1,11 +1,11 @@
 import { all } from "redux-saga/effects";
 
-import dataSagas from "../sagas.js";
+// import dataSagas from "../sagas.js";
 import uiSagas from "./uiSagas.js";
 // Combine sagas solution taken from Andarist's comment at:
 // https://github.com/redux-saga/redux-saga/issues/160
 function* sagas() {
-  yield all([...dataSagas, ...uiSagas]);
+  yield all([...uiSagas]);
 }
 export default sagas;
 
--- src/dux/sagas/uiSagas.js
+++ src/dux/sagas/uiSagas.js
@@ -1,10 +1,14 @@
 import { select, takeLatest } from "redux-saga/effects";
 import { Toast } from "native-base";
 
-import { getShowOnlyFavourites } from "../uiReducer.js";
-import { getFavouritesCount } from "../favouritesReducer.js";
+import { getShowOnlyFavourites, SET_SHOW_FAVOURITES } from "../uiReducer.js";
+import {
+  getFavouritesCount,
+  UPDATE_BAND_FAVOURITES_STATUS
+} from "../favouritesReducer.js";
 
 export function* showFavouritesWarning() {
+  console.log("showFavouritesWarning");
   const state = yield select();
   if (getShowOnlyFavourites(state) && getFavouritesCount(state) === 0) {
     yield Toast.show({
@@ -17,9 +21,24 @@ export function* showFavouritesWarning() {
   }
 }
 
-function* uiSagas() {
-  yield takeLatest("SET_SHOW_FAVOURITES", showFavouritesWarning);
-}
+// function* uiSagas() {
+//   yield takeLatest(SET_SHOW_FAVOURITES, showFavouritesWarning);
+//   yield takeLatest(UPDATE_BAND_FAVOURITES_STATUS, showFavouritesWarning);
+// }
+
+const uiSagas = [
+  takeLatest(SET_SHOW_FAVOURITES, showFavouritesWarning),
+  takeLatest(UPDATE_BAND_FAVOURITES_STATUS, showFavouritesWarning)
+];
 
 export default uiSagas;
 
+/*
+const writeFirebaseSagas = [
+  takeEvery(bandsActionTypes.SAVE_NEW_BAND, saveData),
+  takeEvery(bandsActionTypes.SAVE_EDITED_BAND, saveData),
+  takeEvery(bandsActionTypes.DELETE_BANDS, saveData)
+];
+
+export default writeFirebaseSagas;
+ */

6bfd4e5a8782ee6d32f235c31bc5302f9eb047bf
Michael Brown <brownieboy@gmail.com>
2018-08-22 21:28:06 +1000

    Updated react-native link

--- android/app/src/main/java/com/helstonbury/MainApplication.java
+++ android/app/src/main/java/com/helstonbury/MainApplication.java
@@ -3,6 +3,7 @@ package com.helstonbury;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FastImageViewPackage(),
             new RNFetchBlobPackage(),
             new SplashScreenReactPackage(),
             new RNFetchBlobPackage(),
